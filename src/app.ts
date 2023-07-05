1849b3250e2a6864567b09664c91be4915de91cf
Alberto Valenzuela <avalen28@gmail.com>
2023-07-05 20:04:15 +0200

    adds first class to form rendering

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-// Code goes here!
+class ProjectInput{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement; // it will be a div element
+    element: HTMLFormElement
+
+
+    constructor() {
+      //pointing to the template id = project-input. ! expresión says to Ts that
+      //that tag will exist as a HTML input (div, button,p...)
+      this.templateElement = document.getElementById(
+        'project-input'
+      )! as HTMLTemplateElement;
+      //pointing to the template id = app. ! expresión says to Ts that
+      //that tag will exist as a HTML input (in this case, as an Div)
+      this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    //import from the template, the content (childs).
+      const importedHTMLContent = document.importNode(
+        this.templateElement.content,
+        true
+      );
+        //store the first child that we know that is a Form
+        this.element = importedHTMLContent.firstChild as HTMLFormElement
+        this.attach()
+    }
+//function to attack the content from the FORM into another place on the HTML
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const pjctInput = new ProjectInput();
